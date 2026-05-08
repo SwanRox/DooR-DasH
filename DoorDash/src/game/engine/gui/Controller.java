fc5fe33283c9bd1a96e8be6aac80ab7fce1fbe06
AnasMuharram <anas.o.muharram@gmail.com>
2026-05-08 19:47:19 +0300

    controller foundation

---
+++ DoorDash/src/game/engine/gui/Controller.java
@@ -0,0 +1,13 @@
+package game.engine.gui;
+
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.stage.*;
+
+public class Controller extends Application{
+
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
